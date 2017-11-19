fdb1bfb78a60497fb0efce6fb35e2617addeecd3
eduriol <eduriol@gmail.com>
2017-11-19 13:28:53 +0100

    Second feature simplified

--- src/test/java/stepdefinitions/Steps.java
+++ src/test/java/stepdefinitions/Steps.java
@@ -2,6 +2,8 @@ package stepdefinitions;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import com.bdd.retail.entities.Customer;
 
 import cucumber.api.DataTable;
@@ -15,31 +17,17 @@ public class Steps {
 	String name;
 	String surname;
 	Customer customer;
+	List<Customer> signedUpPeople;
 
 	@Given("^(.+) (.+) wants to become a new customer$")
 	public void wants_to_become_a_new_customer(String name, String surname) {
 	    this.name = name;
 	    this.surname = surname;
 	}
-	
-	@Given("^the following people signed up (\\d+) days? ago:$")
-	public void the_following_people_signed_up_days_ago(int numberOfDaysAgo, DataTable signedUpPeople) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
-	}
 
 	@When("^I signup (?:him|her) into the platform$")
 	public void signup_somebody_into_the_platform() {
 	    customer = new Customer("123456789", name, surname);
-	}
-	
-	@When("^I order a list of customers signed up in the last (\\d+) days?$")
-	public void i_order_a_list_of_customers_signed_up_in_the_last_days(int numberOfDays) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^(?:he|she) starts with (\\d+) points$")
@@ -50,15 +38,6 @@ public class Steps {
 	@Then("^(?:he|she) receives a welcome notification$")
 	public void she_receives_a_welcome_notification() throws PendingException {
 		assertEquals("Welcome to our platform!", customer.getLastNotificationReceived().getTitle());
-	}
-	
-	@Then("^I get the following customers:$")
-	public void i_get_the_following_customers(DataTable expectedCustomers) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    // For automatic transformation, change DataTable to one of
-	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
-	    throw new PendingException();
 	}
 
 }
--- src/test/resources/listRetailCustomers.feature
+++ src/test/resources/listRetailCustomers.feature
@@ -3,22 +3,15 @@ In order to learn about my retail sign up activity
 As a retail manager
 I want to get lists of new customers
 
-Scenario: platform should provide with a list of customers signed up in the last 3 days
-	Given the following people signed up 1 day ago:
-		| name		| surname	|
-		| Marty		| McFly		|
-		| Jennifer	| Parker	|
-	And the following people signed up 2 days ago:
-		| name		| surname	|
-		| Emmett	| Brown		|
-		| Biff		| Tannen	|
-	And the following people signed up 4 days ago:
-		| name		| surname	|
-		| George	| McFly		|
-	When I order a list of customers signed up in the last 3 days
+Scenario: platform should provide with a list of customers signed up today
+	Given the following people signed up:
+		| name		| surname	| sign up date	|
+		| Marty		| McFly		| now			|
+		| Jennifer	| Parker	| now			|
+		| Emmett	| Brown		| 2016-10-25	|
+		| Biff		| Tannen	| 2015-08-31	|
+	When I order a list of customers signed up today
 	Then I get the following customers:
 		| name		| surname	|
 		| Marty		| McFly		|
 		| Jennifer	| Parker	|
-		| Emmett	| Brown		|
-		| Biff		| Tannen	|
