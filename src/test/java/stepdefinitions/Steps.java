f27e471c6304b618ceca1ce8f2c84c042888f9c6
eduriol <eduriol@gmail.com>
2017-11-19 12:43:27 +0100

    Steps defined for secound feature

--- src/test/java/stepdefinitions/Steps.java
+++ src/test/java/stepdefinitions/Steps.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import com.bdd.retail.entities.Customer;
 
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
@@ -20,10 +21,25 @@ public class Steps {
 	    this.name = name;
 	    this.surname = surname;
 	}
+	
+	@Given("^the following people signed up (\\d+) days? ago:$")
+	public void the_following_people_signed_up_days_ago(int numberOfDaysAgo, DataTable signedUpPeople) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
 
 	@When("^I signup (?:him|her) into the platform$")
 	public void signup_somebody_into_the_platform() {
 	    customer = new Customer("123456789", name, surname);
+	}
+	
+	@When("^I order a list of customers signed up in the last (\\d+) days?$")
+	public void i_order_a_list_of_customers_signed_up_in_the_last_days(int numberOfDays) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
 	}
 
 	@Then("^(?:he|she) starts with (\\d+) points$")
@@ -34,6 +50,15 @@ public class Steps {
 	@Then("^(?:he|she) receives a welcome notification$")
 	public void she_receives_a_welcome_notification() throws PendingException {
 		assertEquals("Welcome to our platform!", customer.getLastNotificationReceived().getTitle());
+	}
+	
+	@Then("^I get the following customers:$")
+	public void i_get_the_following_customers(DataTable expectedCustomers) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
 	}
 
 }
